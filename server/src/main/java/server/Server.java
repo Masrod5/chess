7f6c5d3e6fbe7d15bacec7d39edc83f84f81daf5
Masrod5 <masrod@byu.edu>
2025-03-01 11:33:56 -0700

    started phase 3 and adding all the folders and DAO and records

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,15 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.*;
+import record.*;
 import spark.*;
 
 public class Server {
+
+    UserDAO userDAO;
+    AuthDAO authDAO;
+    GameDAO gameDAO;
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -11,11 +18,22 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
+        Spark.post("/user", this::register);
+
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private String register(Request req, Response res) {
+        Gson serialize = new Gson();
+        UserData request = serialize.fromJson(req.body(), UserData.class);
+
+        AuthData auth = Service(userDAO, )
+
+        return null;
     }
 
     public void stop() {
